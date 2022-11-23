84b3ff026eb140109e37b687e68bc0cb2487eb81
dasommmm <103475183+dasommmm@users.noreply.github.com>
2022-11-23 21:51:13 +0900

    11.23

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
-const modal = document.getElementsByClassName('modal');
-const aLush = document.getElementsByClassName('a-lush')
+modalDS();
+
+function modalDS(){
+
+    const modal = document.getElementsByClassName('modal');
+    const openLush = document.getElementsByClassName('open-lush')
+    const closeLush = document.getElementsByClassName('modal-but')
+    
+    const openModal = ()=> {
+        modal[0].classList.remove('modal-hidden');
+    }
+    
+    const closeModal = ()=> {
+        modal[0].classList.add('modal-hidden');
+    }
+    
+    openLush[0].addEventListener("click",openModal);
+    closeLush[0].addEventListener("click",closeModal);
+}
 
 
-modal.hide();
 
