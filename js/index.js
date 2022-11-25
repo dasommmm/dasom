bf494b0b811c505ed909f0968378c4e30cb3eee6
dasommmm <smile6198@naver.com>
2022-11-25 15:00:56 +0900

    1125

--- js/index.js
+++ js/index.js
@@ -3,24 +3,17 @@ makeMove('move-top','0.6s');
 modalDS();
 
 function introMove(){
-    const introText = document.getElementsByClassName('intro-text');
-    const introOne = document.getElementsByClassName('introduce-one');
-    const introTwo = document.getElementsByClassName('introduce-two');
-    const introThree = document.getElementsByClassName('introduce-three');
-    
-    for(let i=0; i<introText.length; i++){
+    const introText = document.querySelector('.intro-text');
+    const intro = document.getElementsByClassName('introduce-text');
+
+    for(let i=0; i<intro.length; i++){
+        intro[i].style.transitionDelay = `${i*0.2}s` 
         window.addEventListener('scroll',()=>{
-            console.log(introText[i].getBoundingClientRect().top - window.innerHeight);
-            if(introText[i].getBoundingClientRect().top - window.innerHeight < 0){
-                introOne[i].classList.add('show');
-                introTwo[i].classList.add('show');
-                introThree[i].classList.add('show');
-                
+            console.log(introText.getBoundingClientRect().top - window.innerHeight);
+            if(introText.getBoundingClientRect().top - window.innerHeight < 0){
+                intro[i].classList.add('show');
             }else{
-                introOne[i].classList.remove('show') ; 
-                introTwo[i].classList.remove('show') ; 
-                introThree[i].classList.remove('show') ; 
-                
+                intro[i].classList.remove('show') ; 
             }
         })
     }
@@ -77,13 +70,11 @@ function makeMove(className,time){
 // open-lush누르면 모달이 뜨는데 처음엔 사진의 맨위부터 뜨다가
 // close 버튼 누르고 다시 누르면  사진의 맨 밑부터 떠용ㅠㅠ
 // 그리고 모달창 끄면 웹사이트가 맨 위 섹션부터 다시 시작해요
-// footer에 z-index를 안주면 모달 위에 footer가 떠요
-// 그런데 z-index를 주니까 a 태그가 안먹혀요.....
 
 function modalDS(){
     const modal = document.getElementsByClassName('modal');
-    const openLush = document.querySelectorAll('.open-lush')
-    const openPaws = document.querySelectorAll('.open-paws')
+    const openLush = document.getElementsByClassName('open-lush')
+    const openPaws = document.getElementsByClassName('open-paws')
     const closeLush = document.querySelector('.lush-but')
     const closePaws = document.querySelector('.paws-but')
 
@@ -94,7 +85,12 @@ function modalDS(){
     
     const closeModalLush = ()=> {
         modal[0].classList.add('modal-hidden');
+        modal[0].scroll({
+            top:modal[0].offsetTop
+        });
     }
+
+    // https://kuzuro.blogspot.com/2018/12/js.html
 
     const openModalPaws = ()=> {
         modal[1].classList.remove('modal-hidden');
